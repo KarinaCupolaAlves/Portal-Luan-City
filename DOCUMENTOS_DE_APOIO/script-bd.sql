5e8277c90f8b1c821d676d6d951203e71765aa0e
KarinaCupolaAlves <karina.alves@sptech.school>
2026-05-23 00:25:16 -0300

    Upload do script de banco de dados.

---
+++ DOCUMENTOS_DE_APOIO/script-bd.sql
@@ -0,0 +1,63 @@
+CREATE DATABASE luan_city;
+USE luan_city;
+
+CREATE TABLE usuario(
+id_usuario INT PRIMARY KEY AUTO_INCREMENT,
+nome VARCHAR(45),
+email VARCHAR(45),
+senha VARCHAR(45),
+telefone CHAR(11),
+dt_nasc DATE
+);
+SELECT * FROM usuario;
+
+CREATE TABLE quiz (
+idQuiz INT PRIMARY KEY AUTO_INCREMENT,
+nomeQuiz VARCHAR(100)
+);
+
+CREATE TABLE resultadoQuiz(
+	idResultado INT PRIMARY KEY AUTO_INCREMENT,
+	fk_usuario INT,
+	fkQuiz INT,
+	pontuação INT,
+	dt_quiz DATETIME,
+	FOREIGN KEY(fk_usuario)
+	REFERENCES usuario(id_usuario),
+	FOREIGN KEY(fkQuiz)
+	REFERENCES quiz(idQuiz)
+	);
+
+ CREATE TABLE usuario_musica (
+  idMusica INT PRIMARY KEY AUTO_INCREMENT,
+  ilha INT,
+  te_esperando INT,
+  ambiente_errado INT,
+  mesmo_sem_estar INT,
+  fk_usuario INT,
+  FOREIGN KEY (fk_usuario) REFERENCES usuario (id_usuario)
+);
+
+CREATE TABLE usuario_personalidade (
+  idPersonalidade INT PRIMARY KEY AUTO_INCREMENT,
+  romantico INT,
+  animado INT,
+  emotivo INT,
+  sofredor INT,
+  caseiro INT,
+  fk_usuario INT,
+  FOREIGN KEY (fk_usuario) REFERENCES usuario (id_usuario)
+);
+SELECT * FROM usuario_personalidade;
+
+INSERT INTO usuario (nome,email,senha,dt_nasc,ja_escutou) VALUES
+('Karina Cupola','karinacupola@gmail.com','karina@123','2027-10-22',true);
+
+INSERT INTO quiz (nomeQuiz) VALUES
+('O quanto você conhece o Luan Santana?'),
+('Qual música do Luan Santana você é?'),
+('Adivinhe a música do Luan Santana pelo trecho	');
+
+
+
+
