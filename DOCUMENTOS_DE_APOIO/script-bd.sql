1ff4277992e1ca47c9be53871b0b3bf079c4762c
KarinaCupolaAlves <karina.alves@sptech.school>
2026-05-28 02:03:42 -0300

    Ajustes no style

--- DOCUMENTOS_DE_APOIO/script-bd.sql
+++ DOCUMENTOS_DE_APOIO/script-bd.sql
@@ -27,6 +27,7 @@ CREATE TABLE resultadoQuiz(
 	FOREIGN KEY(fkQuiz)
 	REFERENCES quiz(idQuiz)
 	);
+SELECT * FROM resultadoQuiz;
 
  CREATE TABLE usuario_musica (
   idMusica INT PRIMARY KEY AUTO_INCREMENT,
@@ -37,18 +38,19 @@ CREATE TABLE resultadoQuiz(
   fk_usuario INT,
   FOREIGN KEY (fk_usuario) REFERENCES usuario (id_usuario)
 );
- 
+ALTER TABLE usuario_musica ADD COLUMN dt_quiz TIMESTAMP DEFAULT CURRENT_TIMESTAMP;
 SELECT * FROM usuario_musica;
+
 CREATE TABLE usuario_personalidade (
   idPersonalidade INT PRIMARY KEY AUTO_INCREMENT,
   romantico INT,
   animado INT,
   emotivo INT,
   sofredor INT,
-  caseiro INT,
   fk_usuario INT,
   FOREIGN KEY (fk_usuario) REFERENCES usuario (id_usuario)
 );
+
 SELECT * FROM usuario_personalidade;
 
 CREATE TABLE usuarioNivelFa(
@@ -84,7 +86,8 @@ SELECT
     um.ambiente_errado, um.mesmo_sem_estar,
     um.fk_usuario, 
     upe.romantico, upe.animado, 
-    upe.emotivo, upe.sofredor, upe.caseiro, 
+    upe.emotivo, upe.sofredor, 
+    upo.dt_quiz,
     unf.pontos AS quiz1,
     upo.pontos AS quiz3
     FROM usuario u
@@ -95,5 +98,54 @@ SELECT
     LEFT JOIN usuarioNivelFa unf
     ON unf.fk_usuario = u.id_usuario	
     LEFT JOIN usuario_pontuacao upo
-    ON upo.fk_usuario = u.id_usuario;
+    ON upo.fk_usuario = u.id_usuario
+	WHERE id_usuario = 17
+    ORDER BY upo.dt_quiz DESC
+    LIMIT 1;
+	
+    SELECT * FROM usuario_musica;
+    UPDATE usuario_pontuacao SET dt_quiz = CURRENT_TIMESTAMP() WHERE idAcertos = (
+    SELECT idAcertos FROM 
+    (SELECT idAcertos FROM usuario_pontuacao WHERE fk_usuario  = 17 ORDER BY dt_quiz DESC LIMIT 1)AS usuario_resultado);
     
+	SELECT 
+    ilha,
+    te_esperando,
+    ambiente_errado,
+    mesmo_sem_estar
+		FROM usuario_musica WHERE fk_usuario = 17
+	ORDER BY dt_quiz DESC
+    LIMIT 1;
+    
+    SELECT 
+    romantico,
+    animado,
+    emotivo,
+    sofredor
+		FROM usuario_personalidade WHERE fk_usuario = 17
+	ORDER BY dt_quiz DESC
+    LIMIT 1;
+    
+    SELECT 
+	 nf.pontos AS quiz1,
+     p.pontos AS quiz3
+		FROM usuario_pontuacao p
+	JOIN usuarioNivelFa nf
+    ON nf.fk_usuario = p.fk_usuario
+        WHERE p.fk_usuario = 17
+	ORDER BY p.dt_quiz DESC
+    LIMIT 1;
+    
+SELECT pontos AS quiz1
+FROM usuarioNivelFa
+WHERE fk_usuario = 17
+ORDER BY dt_quiz DESC
+LIMIT 1;
+
+SELECT pontos AS quiz3
+FROM usuario_pontuacao
+WHERE fk_usuario = 17
+ORDER BY dt_quiz DESC
+LIMIT 1;
+    
+SELECT * FROM usuarioNivelFa  ORDER BY dt_quiz DESC;
